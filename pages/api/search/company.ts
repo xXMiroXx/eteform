38a607ee191ae396986067e9d9136b30d3c68984
Amir <mirooguitar44@gmail.com>
2021-12-06 19:05:57 +0200

    edit fetch api

--- pages/api/search/company.ts
+++ pages/api/search/company.ts
@@ -1,39 +1,55 @@
 import { NextApiHandler } from "next";
-import validator from "../../../helper/search-validator";
-function validateSearchInput(input: string) {
-  if (!input) return { status: false, message: "empty field" };
-  //Check Symbols
-  const symbols = new RegExp(/[^\w\s,.&]|_/);
-  if (input.match(symbols))
-    return {
-      status: false,
-      message: "bad input",
-    };
-  else return { status: true };
-}
+// import validator from "../../../helper/search-validator";
+const validator: (input: string) => [boolean, string] = (input: string) => {
+  input = input.trim();
+  if (!input) return [false, "ادخل اسم الشركه"];
+  if (input.match(/[^a-zA-Z\s&.,]/))
+    return [false, "اسم الشركه يجب ان يكون بالانجليزيه"];
+  return [true, ""];
+};
+// function validateSearchInput(input: string) {
+//   if (!input) return { status: false, message: "empty field" };
+//   //Check Symbols
+//   const symbols = new RegExp(/[^\w\s,.&]|_/);
+//   if (input.match(symbols))
+//     return {
+//       status: false,
+//       message: "bad input",
+//     };
+//   else return { status: true };
+// }
 
-function fetchSearch(name: string) {
-  return fetch(process.env.END_POINT + name, {
-    method: "GET",
-    headers: {
-      Authorization: process.env.API_KEY as string,
-    },
-  }).then((res) => res.json());
+async function fetchSearch(name: string) {
+  try {
+    const res = await fetch(process.env.END_POINT + name, {
+      method: "GET",
+      headers: {
+        Authorization: process.env.API_KEY as string,
+      },
+    });
+    return await res.json();
+  } catch (e) {
+    throw e;
+  }
 }
 
 const handler: NextApiHandler = async (req, res) => {
-  let name = req.query.name as string;
-  // const input = validateSearchInput(name as string);
-  const input = validator(name);
-  if (!input[0]) throw new Error(input[1]);
-  name = name.replace(/ /g, "-");
-  name = encodeURIComponent(name);
-  const data = await fetchSearch(name);
-  const available = !data.total_results;
-  res.status(200).json({
-    status: "success",
-    available,
-  });
+  try {
+    let name = req.query.name as string;
+    // const input = validateSearchInput(name as string);
+    const input = validator(name);
+    if (!input[0]) throw new Error(input[1]);
+    name = name.replace(/ /g, "-");
+    name = encodeURIComponent(name);
+    const data = await fetchSearch(name);
+    const available = !data.total_results;
+    res.status(200).json({
+      status: "success",
+      available,
+    });
+  } catch (e) {
+    res.status(404).json({ message: "something went wrong" });
+  }
 };
 
 export default handler;
--- pages/api/test-same/test-hello.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res
-    .status(200)
-    .json({ message: "test Done", endpoint: process.env.END_POINT });
-}
