92719175b7c5515b26bf9a799cacf9dbfdd808f3
Amir <mirooguitar44@gmail.com>
2021-12-06 18:18:12 +0200

    test endpoint

--- pages/api/test-same/test-hello.ts
+++ pages/api/test-same/test-hello.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: "test Done" });
+  res
+    .status(200)
+    .json({ message: "test Done", endpoint: process.env.END_POINT });
 }
