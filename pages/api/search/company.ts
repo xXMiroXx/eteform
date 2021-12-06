74840c715e55134423e59d92cd4aec93e904bd08
Amir <mirooguitar44@gmail.com>
2021-12-06 19:18:04 +0200

    Edit something to api

--- pages/api/search/company.ts
+++ pages/api/search/company.ts
@@ -35,18 +35,14 @@ async function fetchSearch(name: string) {
 
 const handler: NextApiHandler = async (req, res) => {
   try {
-    let name = req.query.name as string;
-    // const input = validateSearchInput(name as string);
-    const input = validator(name);
-    if (!input[0]) throw new Error(input[1]);
-    name = name.replace(/ /g, "-");
-    name = encodeURIComponent(name);
-    const data = await fetchSearch(name);
-    const available = !data.total_results;
-    res.status(200).json({
-      status: "success",
-      available,
+    const response = await fetch(process.env.END_POINT! + "eteform", {
+      method: "GET",
+      headers: {
+        Authorization: process.env.API_KEY as string,
+      },
     });
+    const data = await response.json();
+    res.status(200).json({ data });
   } catch (e) {
     res.status(404).json({ message: "something went wrong", error: e });
   }
