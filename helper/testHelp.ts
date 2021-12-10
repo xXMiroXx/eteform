eb88f2395f38f85136d522538587e13f3f27b228
Amir <mirooguitar44@gmail.com>
2021-12-10 14:50:07 +0200

    test api 2

--- helper/testHelp.ts
+++ helper/testHelp.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export default function testHelp() {
-  const contact = fs.readFileSync("../data/contact-info.json", {
+  const contact = fs.readFileSync(__dirname + "../data/contact-info.json", {
     encoding: "utf-8",
   });
   console.log(contact);
